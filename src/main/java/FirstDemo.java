05e4769c1378ef11208a1f1764bfe7033b3521c7
KidusMT <kidusmamuye@gmail.com>
2021-05-05 17:13:39 -0500

    updates on web.xml for default pages

--- src/main/java/FirstDemo.java
+++ src/main/java/FirstDemo.java
@@ -1,4 +1,6 @@
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,13 +11,16 @@ import java.io.PrintWriter;
  * @author KidusMT
  * Date: 5/5/2021
  */
-//@WebServlet("/welcome")///FirstServletDemo/welcome
+@WebServlet("/welcome")///FirstServletDemo/welcome
 public class FirstDemo extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Hello Dawg.</h1>");
+        out.println("<h1>Hello Dawg. KidusMT</h1>");
+//        RequestDispatcher view = req.getRequestDispatcher("/web/index.html");
+//        view.forward(req, resp);
     }
+
 }
